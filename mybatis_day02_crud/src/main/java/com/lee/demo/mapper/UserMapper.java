1bfa2dc2a259bad12b6534e9374ac2dd0fbb8f9f
wyliboyu@163.com <wyliboyu@163.com>
2019-01-07 18:01:15 +0800

    dsf

--- mybatis_day02_crud/src/main/java/com/lee/demo/mapper/UserMapper.java
+++ mybatis_day02_crud/src/main/java/com/lee/demo/mapper/UserMapper.java
@@ -2,12 +2,14 @@ package com.lee.demo.mapper;
 
 import com.lee.demo.bean.QueryVO;
 import com.lee.demo.bean.User;
+import org.apache.ibatis.annotations.SelectProvider;
 
 import java.util.List;
 
 public interface UserMapper {
     public List<User> findAll();
 
+//    @SelectProvider(type = "",method = )
     public void saveUser(User user);
 
     public void updateUser(User user);
