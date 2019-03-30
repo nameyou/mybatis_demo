7d09150eceab3d8e0a9713b69f110ea187515991
wyliboyu@163.com <wyliboyu@163.com>
2019-03-30 17:44:32 +0800

    太极测试

---
+++ mybatis_day03_sql/src/test/java/Demo.java
@@ -0,0 +1,22 @@
+public class Demo {
+    public static void main(String[] args) {
+        /*
+            获取最值
+         */
+        getValue(1, 2, 3, "大");
+
+    }
+
+    private static void getValue(int i, int i1, int i2, String str) {
+        if (str.equals("大")) {
+            // TODO 调用获取最大值的方法
+            System.out.println(str);
+        } else if (str.equals("小")) {
+            // TODO 调用获取最小值的方法
+            System.out.println(str);
+        } else {
+            System.out.println("输入字符不合法。。。");
+        }
+    }
+
+}
